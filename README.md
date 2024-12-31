4b7506452941616236b79226fa4e23aadb1494f2
Darshan-12hp <darshanvaishnav05@gmail.com>
2024-12-31 22:09:06 +0530

    Backend Implemented CRUD Operations

---
+++ README.md
@@ -0,0 +1,1 @@
+# shiavnski-assignment
---
+++ springboot-backend/src/test/java/com/shiavnski/ShiavnskiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiavnski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiavnskiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
